207c58a0b5b60bb4461a7c13b0cbd31299a1f0a4
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-08 09:56:45 -0800

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MonoSynthView from './views/MonoSynthView/MonoSynthView';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <MonoSynthView />
+    </>
+  );
+}
---
+++ src/views/MonoSynthView/MonoSynthView.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MonoSynthView() {
+  return <div>MonoSynth</div>;
+}
