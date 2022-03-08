5c924da4d9ed462bc054d0603561f5c4ce70d567
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-08 10:35:12 -0800

    got a simple working synth

--- src/views/MonoSynthView/MonoSynthView.jsx
+++ src/views/MonoSynthView/MonoSynthView.jsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import * as Tone from 'tone';
+
+const keysArray = [
+  'C3',
+  'D3',
+  'E3',
+  'F3',
+  'G3',
+  'A3',
+  'B3',
+  'C4',
+  'D4',
+  'E4',
+  'F4',
+  'G4',
+  'A4',
+  'B4',
+];
 
 export default function MonoSynthView() {
-  return <div>MonoSynth</div>;
+  const synth = new Tone.Synth({
+    oscillator: {
+      type: 'sine2',
+    },
+    envelope: {
+      attack: 0.5,
+    },
+  }).toDestination();
+
+  const handleNote = (note) => {
+    synth.triggerAttackRelease(note, '8n');
+  };
+
+  return (
+    <div>
+      {keysArray.map((note) => (
+        <button key={note} onClick={() => handleNote(note)}>
+          {note}
+        </button>
+      ))}
+    </div>
+  );
 }
