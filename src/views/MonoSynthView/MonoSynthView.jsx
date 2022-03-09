3fdcc3c300ecc6d41fe45415fffcbd87cafb92a7
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-09 13:18:41 -0800

    added some effects and

--- src/views/MonoSynthView/MonoSynthView.jsx
+++ src/views/MonoSynthView/MonoSynthView.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './MonoSynthView.css';
 
 import * as Tone from 'tone';
 
@@ -20,21 +22,62 @@ const keysArray = [
 ];
 
 export default function MonoSynthView() {
+  const [depthLevel, setDepthLevel] = useState(0);
+  const [isGoingUp, setIsGoingUp] = useState(true);
+
+  console.log('depthLevel', depthLevel);
+
+  const feedbackDelay = new Tone.FeedbackDelay(0.525, 0.5).toDestination();
+  // const tremolo = new Tone.Tremolo(depthLevel, 1).toDestination().start();
+
   const synth = new Tone.Synth({
     oscillator: {
-      type: 'sine2',
+      type: 'sawtooth',
     },
     envelope: {
       attack: 0.5,
     },
   }).toDestination();
 
+  useEffect(() => {
+    // if (isGoingUp) {
+    //   setTimeout(() => {
+    //     setDepthLevel((prevState) => prevState + 1);
+    //   }, 200);
+    //   if (depthLevel === 10) {
+    //     setIsGoingUp(false);
+    //   }
+    // } else {
+    //   setTimeout(() => {
+    //     setDepthLevel((prevState) => prevState - 1);
+    //   }, 200);
+    //   if (depthLevel === 1) {
+    //     setIsGoingUp(true);
+    //   }
+    // }
+    synth.disconnect(tremolo);
+    const tremolo = new Tone.Tremolo(depthLevel, 1).toDestination().start();
+    synth.connect(tremolo);
+  }, [depthLevel]);
+
+  synth.chain();
+
   const handleNote = (note) => {
-    synth.triggerAttackRelease(note, '8n');
+    synth.triggerAttackRelease(note, '1n');
   };
 
   return (
     <div>
+      <section>
+        <input
+          type="range"
+          max="10"
+          value={depthLevel}
+          onChange={(e) => {
+            setDepthLevel(Number(e.target.value));
+          }}
+        ></input>
+      </section>
       {keysArray.map((note) => (
         <button key={note} onClick={() => handleNote(note)}>
           {note}
