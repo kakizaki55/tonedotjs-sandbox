6fcc345ecd95b962efda1e9a2acbb642af82638c
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-03-10 11:07:18 -0800

    eh

--- src/views/MonoSynthView/MonoSynthView.jsx
+++ src/views/MonoSynthView/MonoSynthView.jsx
@@ -1,33 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
-import './MonoSynthView.css';
+import style from './MonoSynthView.css';
 
 import * as Tone from 'tone';
 
 const keysArray = [
   'C3',
+  'C#3',
   'D3',
+  'D#3',
   'E3',
   'F3',
+  'F#3',
   'G3',
+  'G#3',
   'A3',
+  'A#3',
   'B3',
   'C4',
+  'C#4',
   'D4',
+  'D#4',
   'E4',
   'F4',
+  'F#4',
   'G4',
+  'G#4',
   'A4',
+  'A#4',
   'B4',
 ];
 
 export default function MonoSynthView() {
   const [depthLevel, setDepthLevel] = useState(0);
   const [isGoingUp, setIsGoingUp] = useState(true);
+  const [feedbackOn, setFeedbackON] = useState(0);
 
   console.log('depthLevel', depthLevel);
 
-  const feedbackDelay = new Tone.FeedbackDelay(0.525, 0.5).toDestination();
   // const tremolo = new Tone.Tremolo(depthLevel, 1).toDestination().start();
 
   const synth = new Tone.Synth({
@@ -55,12 +65,17 @@ export default function MonoSynthView() {
     //     setIsGoingUp(true);
     //   }
     // }
+
     synth.disconnect(tremolo);
     const tremolo = new Tone.Tremolo(depthLevel, 1).toDestination().start();
     synth.connect(tremolo);
   }, [depthLevel]);
 
-  synth.chain();
+  useEffect(() => {
+    synth.disconnect(feedbackDelay);
+    const feedbackDelay = new Tone.FeedbackDelay(0.525, 0.5).toDestination();
+    synth.connect(feedbackDelay);
+  }, [feedbackOn]);
 
   const handleNote = (note) => {
     synth.triggerAttackRelease(note, '1n');
@@ -69,20 +84,54 @@ export default function MonoSynthView() {
   return (
     <div>
       <section>
-        <input
-          type="range"
-          max="10"
-          value={depthLevel}
-          onChange={(e) => {
-            setDepthLevel(Number(e.target.value));
-          }}
-        ></input>
+        <label>
+          tremolo depth
+          <input
+            type="range"
+            max="10"
+            value={feedbackOn}
+            onChange={(e) => {
+              setFeedbackON(Number(e.target.value));
+            }}
+          />
+        </label>
+        <label>
+          FeedbackDelay
+          <input
+            type="range"
+            max="1"
+            value={depthLevel}
+            onChange={(e) => {
+              setDepthLevel(Number(e.target.value));
+            }}
+          />
+        </label>
       </section>
-      {keysArray.map((note) => (
-        <button key={note} onClick={() => handleNote(note)}>
-          {note}
-        </button>
-      ))}
+
+      <div>
+        {keysArray.map((note) => {
+          if (note.length === 3)
+            return (
+              <button
+                className={style.blackKeys}
+                key={note}
+                onClick={() => handleNote(note)}
+              >
+                {note}
+              </button>
+            );
+          else
+            return (
+              <button
+                className={style.whiteKeys}
+                key={note}
+                onClick={() => handleNote(note)}
+              >
+                {note}
+              </button>
+            );
+        })}
+      </div>
     </div>
   );
 }
